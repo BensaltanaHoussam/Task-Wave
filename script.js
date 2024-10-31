5a727ee5b4f24869324945df4dd899391fb5b742
BensaltanaHoussam <bensaltna07@gmail.com>
2024-11-01 00:39:59 +0100

    showing data on to do list

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let title = document.getElementById("title");
 let dueDate =document.getElementById("dueDate");
 let description =document.getElementById("description");
 let msg =document.getElementById("msg");
-
+let todoContainer =document.getElementById("todoContainer");
 
 
 btn.onclick = function() {
@@ -49,8 +49,29 @@ let acceptData = () => {
 data["text"]=title.value;
 data["date"]=dueDate.value;
 data["description"]=description.value;
-console.log(data);
+creatTasks();
 };
+
+
+//affiche data
+
+let creatTasks = ()=>{
+    todoContainer.innerHTML += `
+    <div class="bg-[#16171B] text-xs p-4 ml-2 mr-2 flex flex-col gap-2 text-white rounded-2xl transition-transform duration-300 hover:scale-105">
+                    <span class="text-lg font-bold">${data.text}</span>
+                    <span>${data.date}</span>
+                    <p>${data.description}</p>
+                    <span class="options flex justify-end gap-4 hover:cursor-pointer ">
+                    
+                        <i class="fa-solid fa-trash hover:text-pink-400"></i>
+                        <i class="fa-solid fa-pen-to-square hover:text-pink-400"></i>
+
+                    </span>
+                </div>
+    `;
+
+}
+
 
 
 
